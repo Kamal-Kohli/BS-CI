9656273defbd2682fe5c3b853406df41c6a8b4ff
Kamal-Kohli <>
2023-05-29 07:41:50 +0000

    Add game variables

---
+++ battleship.py
@@ -0,0 +1,42 @@
+import random
+"""
+Legend:
+1. "." = Water or empty space.
+2. "s" = Ship positions, part of the ship.
+3. "o" = Water that was shot with bullets, a miss it hit no ship.
+4. "x" = Ship sunk!
+
+Battleship:
+A. grid = 10x10
+B. Player will have 50 bullets to take down ships.
+C. 5 ships of variable length randomly placed.
+D. Every hit and misses shot will be show up in the grid.
+E. If all ships are unearthed before using up all bullets, You Win else, You Lose.
+F. You can choose a row and column such as A:3, B:5 to indicate where to shoot.
+"""
+
+# Global constants
+# Global variable for grid
+GRID = [[]]
+
+#Global variable for grd size
+GRID_SIZE = 10
+
+#Global variable of number of ships to place
+NUM_SHIPS = 5
+
+#Global variable for bullets left
+BULLETS_LEFT = 50
+
+#Global variable for game over
+GAME_OVER = False
+
+#Global varibale for ships sunk
+NUM_OF_SHIPS_SUNK = 0
+
+#Gloabal varibale for ship positions
+SHIP_POSITIONS = [[]]
+
+
+# Create the game board
+myBoard = [['.' for _ in range(GRID_SIZE)] for _ in range(GRID_SIZE)]
